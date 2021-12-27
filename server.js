72247c87dd9d1a92726e70d1e908c9dbb742b1aa
unknown <chadiseif96@gmail.com>
2021-12-27 08:20:11 +0100

    add path in heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const connectDb = require("./config/connect");
 const mysql = require("mysql");
+const path = require("path");
 
 require("dotenv").config();
 
